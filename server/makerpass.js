27e0bca7661a78ec4c1047ce9d1af1d003c1e416
Dan Corman <dan.corman@gmail.com>
2015-06-17 19:11:47 -0500

    working on schema

--- server/makerpass.js
+++ server/makerpass.js
@@ -11,7 +11,6 @@ var School     = require('./models/school')
 
 exports.mount = function (app, host) {
 
-
   passport.use(new MakerpassStrategy({
       clientID: process.env.MAKERPASS_CLIENT_ID || 'eae6795ed5d6fe1fb29497641a083edb2c4fe242a233fc98138f7224177581e9',
       clientSecret: process.env.MAKERPASS_CLIENT_SECRET || 'c92309a053636df7b24e1672ce6190ef9c76bca9e5aebf1cb8847e70607b534f',
