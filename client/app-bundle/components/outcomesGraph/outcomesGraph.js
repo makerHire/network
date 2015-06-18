0c8d3c7578019a35584199d0ba1c0839ecd3cc8d
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-18 15:15:41 -0500

    Outcomes graph stubbed out

---
+++ client/app-bundle/components/outcomesGraph/outcomesGraph.js
@@ -0,0 +1,23 @@
+var m = require('mithril');
+
+exports.controller = function () {
+  var ctrl = this;
+}
+
+exports.view = function (ctrl) {
+  return m('graphContainer', [
+    m('.graph', [
+      m('h1', "Student Progress"),
+      m('.graph', "This will be a graph"),
+    ]),
+    m('.graph', [
+      m('h1', "Other Progress"),
+      m('.graph', "This will be another graph"),
+    ]),
+    m('.graph', [
+      m('h1', "Third Progress"),
+      m('.graph', "This will be the third graph"),
+    ])
+  ])
+
+}
