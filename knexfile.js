349ad07f8e83045dead73285e8419eee0e6a8daf
Dan Corman <dan.corman@gmail.com>
2015-06-18 11:51:25 -0500

    Working on DB talking

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,13 @@
 
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: {
-      host: 'ec2-23-23-188-252.compute-1.amazonaws.com',
-      database: 'd7dub96b5jqr4l'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  test: {
-    client: 'postgresql',
-    connection: {
-      database: 'learn_test'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
+  
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'learn',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: 'postgres://yuofkehmpkcdbk:D15Qec1JRfhWn_vE7-CIbsbo28@ec2-54-83-43-118.compute-1.amazonaws.com:5432/d8h2gnum0his57',
     migrations: {
       tableName: 'knex_migrations'
     }
-  },
+  }
 
 };
