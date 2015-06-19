b889bd898c24e24998ecc2237bd119ae7c6fd37a
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-19 15:01:59 -0500

    Added all forms for tracking students

---
+++ client/app-bundle/components/newApp/newApp.js
@@ -0,0 +1,67 @@
+var m = require('mithril');
+
+exports.controller = function () {
+  var ctrl = this;
+}
+
+exports.view = function (ctrl) {
+  return m('.row', [
+    m('.row', [
+      m('h3.center-align', 'Initial Application')
+    ]),
+    m('form.col.s12', [
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Company]'),
+          m('label[for=first_name]', "Company")
+        ]),
+        m('.input-field.col.s12.m6', [
+          //Should auto complete for common companies
+          m('input#first_name.validate[type=text][placeholder=Location]'),
+          m('label[for=first_name]', "Location")
+        ])
+      ]),
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder="Company Website"]'),
+          m('label[for=first_name]', "Company Website")
+        ]),
+        m('.input-field.col.s12.m6', [
+          //Should auto complete for common jobs
+          m('input#first_name.validate[type=text][placeholder="Job Title"]'),
+          m('label[for=first_name]', "Job Title")
+        ])
+      ]),
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          //Should have a limit of text
+          m('input#first_name.validate[type=text][placeholder="How it Became a Lead"]'),
+          m('label[for=first_name]', "How it Became a Lead")
+        ]),
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Application Method]'),
+          //Should autocomplete for common methods
+          m('label[for=first_name]', "Application Method")
+        ])
+      ]),
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          //Should have a limit of text
+          m('input#first_name.datepicker[type=date][placeholder="Date Applied"]'),
+          m('label[for=first_name]', "Date Applied")
+        ]),
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Application Method]'),
+          //Should autocomplete for common methods
+          m('label[for=first_name]', "Application Method")
+        ])
+      ]),
+      m('.row', [
+        m('button.btn.waves-effect.waves-light', 'Submit',[
+          //POST to database
+          m('i.mdi-content-send.right')
+        ])
+      ])
+    ])
+  ])
+}
