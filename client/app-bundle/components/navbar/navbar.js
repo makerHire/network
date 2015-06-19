09a2f3799e32ef69c2ca289cf779cca2cc56c34b
sbraunstein <sbraunstein12@gmail.com>
2015-06-18 19:30:11 -0500

    navBar needing materialize fix

--- client/app-bundle/components/navbar/navbar.js
+++ client/app-bundle/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 // require('./ext/functions.js')
-
 var m = require('mithril')
+
 
 exports.controller = function () {
 	var ctrl = this;
@@ -8,14 +8,27 @@ exports.controller = function () {
 
 exports.view = function (ctrl) {
 	console.log('navbar-view')
-	  return m("html", [
-        m("body", [
-            m("ul", [
-            	m("a#[href='http://localhost:4000/?/']", "Home"),
-            	m("a#google.external[href='http://localhost:4000/?/profile']", "Student"),
-            	m("a#google.external[href='http://localhost:4000/?/page']", "Crew"),
-            	m("a#google.external[href='http://localhost:4000/?/']", "AboutUs")
-               ])
-            ])
-        ]);
-	}
+	return m("nav", [
+		m(".nav-wrapper", [
+			m("ul", [
+				m("a#brand-logo[href='#']", "MakerHire"),
+				m("ul", {id: "nav-mobile"}, {class: "right hide-on-med-and-down"}, [
+					m('li', "a[href='sass.html']"),
+					m('li', "a[href='components.html']"),
+					m('li', "a[href='javascript.html']")
+					])
+				])
+			])
+		])
+}
+
+	 //  <nav>
+  //   <div class="nav-wrapper">
+  //     <a href="#" class="brand-logo">Logo</a>
+  //     <ul id="nav-mobile" class="right hide-on-med-and-down">
+  //       <li><a href="sass.html">Sass</a></li>
+  //       <li><a href="components.html">Components</a></li>
+  //       <li><a href="javascript.html">JavaScript</a></li>
+  //     </ul>
+  //   </div>
+  // </nav>
