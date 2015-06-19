2c17023dd2334b63ecacc988527644914a4ef459
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-19 15:01:59 -0500

    Added all forms for tracking students

--- client/app-bundle/components/messaging/messaging.js
+++ client/app-bundle/components/messaging/messaging.js
@@ -5,5 +5,22 @@ exports.controller = function () {
 }
 
 exports.view = function (ctrl) {
-  return m('div', 'messaging here');
+  return m('.row', [
+    m('form', {class: "col s12"}, [
+      m('.row', [
+        m('.row', {class: "input-field col l6 m6 s12"}, [
+          m('i', {class: "mdi-editor-mode-edit prefix"}),
+          m('textarea#icon_prefix2', {class: "materialize-textarea"}),
+          m('label[for=icon_prefix2]', "Message")
+        ]),
+        m('.row', [
+          m('button.btn.waves-effect.waves-light', 'Send Message',[
+            //POST to database
+            m('i.mdi-content-send.right')
+          ])
+        ])
+
+      ])
+    ])
+  ])
 }
--- client/app-bundle/views/studentProfile/studentProfile.js
+++ client/app-bundle/views/studentProfile/studentProfile.js
@@ -1,16 +1,21 @@
-var m = require('mithril')
+var m             = require('mithril');
 var Navbar        = require('../../components/navbar/navbar.js');
 var NewApp        = require('../../components/newApp/newApp.js');
 var Messaging     = require('../../components/messaging/messaging.js');
+var Phases        = require('../../components/phases/phases.js');
+var PhoneScreen   = require('../../components/phoneScreen/phoneScreen.js');
+
 
 exports.controller = function () {
-    var ctrl = this
+  var ctrl = this;
 }
 
 exports.view = function (ctrl) {
   return m('.profile', [
     m.component(Navbar),
     m.component(NewApp),
+    m.component(PhoneScreen),
+    m.component(Phases),
     m.component(Messaging)
   ])
 }
