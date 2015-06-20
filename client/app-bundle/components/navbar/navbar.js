8bc542aa77a1587f5693e451c46d06905486749e
sbraunstein <sbraunstein12@gmail.com>
2015-06-19 19:13:25 -0500

    Corrected navbar routes

--- client/app-bundle/components/navbar/navbar.js
+++ client/app-bundle/components/navbar/navbar.js
@@ -27,3 +27,14 @@ exports.view = function (ctrl) {
 		])
 	])
 }
+
+  // <nav>
+  //   <div class="nav-wrapper">
+  //     <a href="#" class="brand-logo">Logo</a>
+  //     <ul id="nav-mobile" class="right hide-on-med-and-down">
+  //       <li><a href="sass.html">Sass</a></li>
+  //       <li><a href="components.html">Components</a></li>
+  //       <li><a href="javascript.html">JavaScript</a></li>
+  //     </ul>
+  //   </div>
+  // </nav>
