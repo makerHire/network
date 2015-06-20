d9065326d4164a99a9cfc58639e98808d2f6acb4
Dan Corman <dan.corman@gmail.com>
2015-06-20 14:47:00 -0500

    deployed DB path

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,7 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: {
-      database: 'learn_dev'
-    },
+    connection: 'postgres://yuofkehmpkcdbk:D15Qec1JRfhWn_vE7-CIbsbo28@ec2-54-83-43-118.compute-1.amazonaws.com:5432/d8h2gnum0his57',
     pool: {
       min: 2,
       max: 10
@@ -31,11 +29,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'learn',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 'postgres://yuofkehmpkcdbk:D15Qec1JRfhWn_vE7-CIbsbo28@ec2-54-83-43-118.compute-1.amazonaws.com:5432/d8h2gnum0his57',
     pool: {
       min: 2,
       max: 10
@@ -43,6 +37,5 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
-  },
-
+  }
 };
