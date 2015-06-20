de9d7bf573db92739ce528d833f74ff5902ca7f3
Dan Corman <dan.corman@gmail.com>
2015-06-20 14:39:05 -0500

    fixed db path

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 
 var config = require('../knexfile.js')
-var env = 'development'
+var env = process.env.NODE_ENV || 'development'
 var knex = require('knex')(config[env])
 var Promise = require('knex/node_modules/bluebird')
 
