060339c3e686e57c656c7cd79c827bf669edfb61
Dan Corman <dan.corman@gmail.com>
2015-06-22 20:32:44 -0500

    added titles table

--- migrations/20150521143446_create_makerpass_tables.js
+++ migrations/20150521143446_create_makerpass_tables.js
@@ -1,222 +1,112 @@
 
--- ---
--- Globals
--- ---
+exports.up = function(knex, Promise) {
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+  console.log('hello')
 
--- ---
--- Table 'School'
--- 
--- ---
+  return Promise.all([
+    knex.schema.createTable('users', function (table) {
 
-DROP TABLE IF EXISTS `School`;
-    
-CREATE TABLE `School` (
-  `uid` VARCHAR(255) NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `location` VARCHAR(255) NULL DEFAULT NULL,
-  `url` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-);
+      table.string('uid').primary()
+      table.string('name')
+      table.string('email')
+      table.string('avatar_url')
+      table.string('status')
+      table.string('gender')
+      
+      table.timestamps()
+    }),
 
--- ---
--- Table 'Groups'
--- 
--- ---
+    knex.schema.createTable('groups', function (table) {
 
-DROP TABLE IF EXISTS `Groups`;
-    
-CREATE TABLE `Groups` (
-  `uid` VARCHAR(255) NULL AUTO_INCREMENT DEFAULT NULL,
-  `school_uid` VARCHAR(255) NULL DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `role` VARCHAR(255) NULL DEFAULT NULL,
-  `info` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-);
+      table.string('uid').primary()
+      table.string('school_uid').references('uid').inTable('schools')
+      table.string('name_id')
+      table.string('name')
+      table.string('role')
+      table.json('info')
 
--- ---
--- Table 'User'
--- 
--- ---
+      table.timestamps()
+    }),
 
-DROP TABLE IF EXISTS `User`;
-    
-CREATE TABLE `User` (
-  `uid` VARCHAR(255) NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `email` VARCHAR(255) NULL DEFAULT NULL,
-  `avatar_url` VARCHAR(255) NULL DEFAULT NULL,
-  `status` VARCHAR(255) NULL DEFAULT NULL,
-  `gender` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-);
+    knex.schema.createTable('phases', function (table) {
 
--- ---
--- Table 'Membership'
--- 
--- ---
+      table.string('uid').primary()
+      table.string('name')
+      table.string('phase').references('uid').inTable('phases').notNullable()
+      table.string('users').references('uid').inTable('users').notNullable()
+      table.string('school').references('uid').inTable('schools').notNullable()
 
-DROP TABLE IF EXISTS `Membership`;
-    
-CREATE TABLE `Membership` (
-  `uid` VARCHAR NULL AUTO_INCREMENT DEFAULT NULL,
-  `user_uid` VARCHAR(255) NULL DEFAULT NULL,
-  `group_uid` VARCHAR(255) NULL DEFAULT NULL,
-  `role` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-);
+      table.timestamps()
+    }),
 
--- ---
--- Table 'Job'
--- 
--- ---
+    knex.schema.createTable('schools', function (table) {
 
-DROP TABLE IF EXISTS `Job`;
-    
-CREATE TABLE `Job` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `company_id` INTEGER NULL DEFAULT NULL,
-  `job_id` INTEGER NULL DEFAULT NULL,
-  `salary` INTEGER NULL DEFAULT NULL,
-  `start_date` DATETIME NULL DEFAULT NULL,
-  `user_id` VARCHAR(255) NULL DEFAULT NULL,
-  `end_date` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+      table.string('uid').primary()
+      table.string('name')
+      table.string('name_id')
+      table.string('location')
+      table.string('url')
 
--- ---
--- Table 'Companies'
--- 
--- ---
+      table.timestamps()
+    }),
 
-DROP TABLE IF EXISTS `Companies`;
-    
-CREATE TABLE `Companies` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `url` VARCHAR(255) NULL DEFAULT NULL,
-  `address` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+    knex.schema.createTable('jobs', function (table) {
 
--- ---
--- Table 'Apps'
--- 
--- ---
+      table.string('uid').primary()
+      table.string('user')
+      table.string('title')
+      table.dateTime('date_applied')
+      table.string('company').references('uid').inTable('companies')
+      table.string('contact')
+      table.string('app_method')
+      table.integer('title_id').references('id').inTable('titles')
+      tavle.string('user_id').references('uid').inTable('users')
+      table.boolean('active')
+      table.json('details')
 
-DROP TABLE IF EXISTS `Apps`;
-    
-CREATE TABLE `Apps` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Status` VARCHAR(255) NULL DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL,
-  `contact_id` INTEGER NULL DEFAULT NULL,
-  `date_applied` DATETIME NULL DEFAULT NULL,
-  `job_id` INTEGER NULL DEFAULT NULL,
-  `Phases` VARCHAR(255) NULL DEFAULT NULL,
-  `new field` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+      table.timestamps()
+    }),
 
--- ---
--- Table 'Company Contacts'
--- 
--- ---
 
-DROP TABLE IF EXISTS `Company Contacts`;
-    
-CREATE TABLE `Company Contacts` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `phone_number` VARCHAR(255) NULL DEFAULT NULL,
-  `company_id` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
 
--- ---
--- Table 'Job Type'
--- 
--- ---
+    knex.schema.createTable('titles', function(table){
 
-DROP TABLE IF EXISTS `Job Type`;
-    
-CREATE TABLE `Job Type` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `Title` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+      table.integer.increments('id').primary();
+      table.string('title')
 
--- ---
--- Table 'Interviews'
--- 
--- ---
+      table.timestamps()
+    }),
 
-DROP TABLE IF EXISTS `Interviews`;
-    
-CREATE TABLE `Interviews` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `app_id` INTEGER NULL DEFAULT NULL,
-  `info` VARCHAR(500) NULL DEFAULT NULL,
-  `date` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+    knex.schema.createTable('memberships', function (table) {
 
--- ---
--- Foreign Keys 
--- ---
+      table.string('uid').primary()
+      table.string('user_uid').references('uid').inTable('users').notNullable()
+      table.string('group_uid').references('uid').inTable('groups').notNullable()
+      table.string('role')
 
-ALTER TABLE `Groups` ADD FOREIGN KEY (school_uid) REFERENCES `School` (`uid`);
-ALTER TABLE `Membership` ADD FOREIGN KEY (user_uid) REFERENCES `User` (`uid`);
-ALTER TABLE `Membership` ADD FOREIGN KEY (group_uid) REFERENCES `Groups` (`uid`);
-ALTER TABLE `Job` ADD FOREIGN KEY (company_id) REFERENCES `Companies` (`id`);
-ALTER TABLE `Job` ADD FOREIGN KEY (job_id) REFERENCES `Job Type` (`id`);
-ALTER TABLE `Job` ADD FOREIGN KEY (user_id) REFERENCES `User` (`uid`);
-ALTER TABLE `Apps` ADD FOREIGN KEY (user_id) REFERENCES `User` (`uid`);
-ALTER TABLE `Apps` ADD FOREIGN KEY (contact_id) REFERENCES `Company Contacts` (`id`);
-ALTER TABLE `Apps` ADD FOREIGN KEY (job_id) REFERENCES `Job Type` (`id`);
-ALTER TABLE `Company Contacts` ADD FOREIGN KEY (company_id) REFERENCES `Companies` (`id`);
-ALTER TABLE `Interviews` ADD FOREIGN KEY (app_id) REFERENCES `Apps` (`id`);
+      table.timestamps()
+    }),
 
--- ---
--- Table Properties
--- ---
+    knex.schema.createTable('companies', function (table) {
 
--- ALTER TABLE `School` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Groups` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `User` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Membership` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Job` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Companies` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Apps` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Company Contacts` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Job Type` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Interviews` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+      table.string('uid').primary()
+      table.string('name')
+      table.string('url')
+      table.string('location')
+      table.string('app_method')
+      table.string('contacts')
+      table.string('status').references('uid').inTable('phases')
+      table.boolean('active')
 
--- ---
--- Test Data
--- ---
+      table.timestamps()
+    })
+  ])
+}
 
--- INSERT INTO `School` (`uid`,`name`,`location`,`url`) VALUES
--- ('','','','');
--- INSERT INTO `Groups` (`uid`,`school_uid`,`name`,`role`,`info`) VALUES
--- ('','','','','');
--- INSERT INTO `User` (`uid`,`name`,`email`,`avatar_url`,`status`,`gender`) VALUES
--- ('','','','','','');
--- INSERT INTO `Membership` (`uid`,`user_uid`,`group_uid`,`role`) VALUES
--- ('','','','');
--- INSERT INTO `Job` (`id`,`company_id`,`job_id`,`salary`,`start_date`,`user_id`,`end_date`) VALUES
--- ('','','','','','','');
--- INSERT INTO `Companies` (`id`,`name`,`url`,`address`) VALUES
--- ('','','','');
--- INSERT INTO `Apps` (`id`,`Status`,`user_id`,`contact_id`,`date_applied`,`job_id`,`Phases`,`new field`) VALUES
--- ('','','','','','','','');
--- INSERT INTO `Company Contacts` (`id`,`name`,`phone_number`,`company_id`) VALUES
--- ('','','','');
--- INSERT INTO `Job Type` (`id`,`Title`) VALUES
--- ('','');
--- INSERT INTO `Interviews` (`id`,`app_id`,`info`,`date`) VALUES
--- ('','','','');
-
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('memberships'),
+    knex.schema.dropTable('groups'),
+    knex.schema.dropTable('users')
+  ])
+}
