c251d57c4f3bcd483f69c7cbe43518c583338d3a
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-23 11:25:59 -0500

    Navbar does not flash on page change

--- client/app-bundle/components/navbar/navbar.js
+++ client/app-bundle/components/navbar/navbar.js
@@ -9,32 +9,21 @@ exports.controller = function () {
 exports.view = function (ctrl) {
 	var links = [
     {title: "Home", url: "/"},
-    {title: "Outcomes", url: "?/outcomes"},
-    {title: "Students", url: "?/profile"}
-];
+    {title: "Outcomes", url: "/outcomes"},
+    {title: "Students", url: "/profile"}
+  ];
 	console.log('navbar-view')
 	return m("nav", [
 		m('.nav-wrapper', [
 			m("a[href='#']#brand-logo", "MakerHire", [
 			m("ul#nav-mobile.right.hide-on-med-and-down", [
 				m('li', links.map(function(link) {
-            		return m("li",
-                		m("a", {href: link.url}, link.title) 
-                	)}
-                	))
-            	])
+      		return m("li",
+          		m("a", { href: link.url, config: m.route }, link.title) 
+          	)}
+          ))
+      	])
 			])
 		])
 	])
 }
-
-  // <nav>
-  //   <div class="nav-wrapper">
-  //     <a href="#" class="brand-logo">Logo</a>
-  //     <ul id="nav-mobile" class="right hide-on-med-and-down">
-  //       <li><a href="sass.html">Sass</a></li>
-  //       <li><a href="components.html">Components</a></li>
-  //       <li><a href="javascript.html">JavaScript</a></li>
-  //     </ul>
-  //   </div>
-  // </nav>
