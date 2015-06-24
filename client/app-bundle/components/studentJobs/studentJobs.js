78370e308c9d3d2c7c8226df1a389f1502e725d1
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-24 16:22:28 -0500

    Made avatar circle responsive to user

--- client/app-bundle/components/studentJobs/studentJobs.js
+++ client/app-bundle/components/studentJobs/studentJobs.js
@@ -6,26 +6,10 @@ exports.controller = function () {
   ctrl.studentInfo;
   ctrl.companies;
 
-  // var mHttp = function(method, url, promise, reqData) {
-  //   m.request({ method: method, url: url})
-  //     .then(promise)
-  // };
-  //TODO: Make work for post request and data - jh
-  // mHttp('GET', '/api/jobs', function(jobs) {
-  //     ctrl.studentJobs = m.prop();
-  //     return ctrl.studentJobs(jobs['Jobs']);
-  //   })
-
-  // m.request({ method: 'GET', url: '/api/appswithcompanies'})
-  //   .then(function(applications) {
-  //     ctrl.studentApps = m.prop();
-  //     console.log(applications,'Applications GET')
-  //     return ctrl.studentApps(applications.Applications);
-  //   });
-
   m.request({ method: 'GET', url: '/me'})
     .then(function(info) {
       ctrl.studentInfo = m.prop();
+      console.log(info.user)
       return ctrl.studentInfo(info.user);
     }).then(function(user){
       m.request({ method: 'GET', url: '/api/appswithcompanies', data: user.uid})
@@ -39,8 +23,6 @@ exports.controller = function () {
 
 };
 
-//TODO: join query for company name using company_id
-
 
 exports.view = function (ctrl) {
   return m('.col.m9.s12', [
@@ -49,7 +31,7 @@ exports.view = function (ctrl) {
       ctrl.studentApps().map(function(app){
         if(app.user_id === ctrl.studentInfo().uid){ 
           return m('li.collection-item avatar', [
-            m('img[src=https://avatars.githubusercontent.com/u/5953350?v=3].circle'),
+            m('img[src=' + ctrl.studentInfo().avatar_url + '].circle'),
             m('span.title', app.name),
             m('p', 'Title: ' + app.title),
             m('p', 'Active: ' + app.active),
