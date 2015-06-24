6156e3235601f2e9d5c3630c051deb9ca82477a6
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-24 16:13:46 -0500

    Passing uid directly to databaes on GET /me

--- client/app-bundle/views/outcomes/outcomes.js
+++ client/app-bundle/views/outcomes/outcomes.js
@@ -3,6 +3,7 @@ var Navbar         = require('../../components/navbar/navbar.js')
 var Phases         = require('../../components/phases/phases.js')
 var OutcomesGraph  = require('../../components/outcomesGraph/outcomesGraph.js')
 var Messaging      = require('../../components/messaging/messaging.js')
+var CurrentApps        = require('../../components/currentApps/currentApps.js')
 
 exports.controller = function() {}
 
@@ -11,7 +12,8 @@ exports.view = function(ctrl) {
   return m('.container', [
     m.component(OutcomesGraph),
     m.component(Phases),
-    m.component(Messaging)
+    m.component(Messaging),
+    m.component(CurrentApps)
   ])
 }
 
