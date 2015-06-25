4d6175b8d3d5f1fa460051394e1fbb9d1f72e0b2
Dan Corman <dan.corman@gmail.com>
2015-06-25 10:55:21 -0500

    Endpoint bug fix

--- server/API/applicationsAPI.js
+++ server/API/applicationsAPI.js
@@ -29,7 +29,7 @@ exports.mount = function (app) {
 
   app.get('/API/allApps', function(req,res){
     if (!req.body) return res.sendStatus(400);
-      Applications.retrieveWithCompany(req.user, function(x){res.send({Applications: x})
+      Applications.retrieveAllWithCompany(function(x){res.send({Applications: x})
     })
   });
 
