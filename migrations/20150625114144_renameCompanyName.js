a01033f2a7d5f334e31670cbbcfde80d7916d8da
Dan Corman <dan.corman@gmail.com>
2015-06-25 11:46:04 -0500

    Added console log for migration

--- migrations/20150625114144_renameCompanyName.js
+++ migrations/20150625114144_renameCompanyName.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   
 	return Promise.all([
-
+		console.log('adding company_name migration')
 
 		knex.schema.table('companies', function (table) {
 			table.renameColumn('name', 'company_name');
